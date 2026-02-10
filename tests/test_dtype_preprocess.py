f308acb4c23eaef88c6d7d1723d567a38b569ca2
Miguel Franco UAO <miguel.franco.r01@gmail.com>
2026-02-09 19:29:27 -0500

    Update grad_cam and tests

---
+++ tests/test_dtype_preprocess.py
@@ -0,0 +1,10 @@
+import numpy as np
+from src_directory.preprocess_img import preprocess
+
+def test_preprocess_dtype():
+    """En esta función se realiza una validación del tipo de dato que retorna preprocess, el cual debe ser float32"""
+    img = np.zeros((512, 512, 3), dtype=np.uint8)
+
+    result = preprocess(img)
+
+    assert result.dtype == np.float32
--- tests/test_shape_preprocess.py
+++ tests/test_shape_preprocess.py
@@ -2,6 +2,7 @@ import numpy as np
 from src_directory.preprocess_img import preprocess
 
 def test_preprocess_shape():
+    """Se realiza una verificación del tamaño del tensor que retorna la función preprocess, el cual debe ser (1, 512, 512, 1)"""
     img = np.zeros((300, 300, 3), dtype=np.uint8)
 
     result = preprocess(img)
