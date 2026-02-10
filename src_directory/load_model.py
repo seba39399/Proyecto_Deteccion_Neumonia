225a5bcd5abc81847168071f1add26490d580a5f
AdrianVR199 <adrian.vargas@uao.edu.co>
2026-02-10 11:54:37 -0500

    docs: mejorar documentación de la función model_fun
    Se añadió un docstring detallado con parámetros y retornos

--- src_directory/load_model.py
+++ src_directory/load_model.py
@@ -1,6 +1,21 @@
 import tensorflow as tf
 
 def model_fun():
-    """Carga el modelo una sola vez."""
+    """
+    Carga un modelo entrenado de TensorFlow/Keras desde un archivo .h5.
+
+    Args:
+        path_model (str):
+            Ruta del archivo del modelo en formato .h5.
+
+    Returns:
+        tf.keras.Model:
+            Modelo de Keras cargado y listo para inferencia.
+
+    Example:
+        model = model_fun("conv_MLP_84.h5")
+    """
+    
     model = tf.keras.models.load_model('conv_MLP_84.h5', compile=False)
     return model
+
